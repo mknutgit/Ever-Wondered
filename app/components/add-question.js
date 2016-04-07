40395805341ee218141d301690e0567e74abe06f
matt knutson <mknut001@gmail.com>
2016-04-07 15:42:11 -0700

    add questions to the list

--- app/components/add-question.js
+++ app/components/add-question.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+    forgetIt() {
+      this.set('addNewQuestion', false);
+    },
+    addQuestion() {
+      var params = {
+        question: this.get('question') ? this.get('question'): "",
+        author: this.get('author') ? this.get('author'): "",
+        notes: this.get('notes') ? this.get('notes'): ""
+      };
+      this.set('addNewQuestion', false);
+      this.set('question', '');
+      this.set('author', '');
+      this.set('notes', '');
+      this.sendAction('addQuestion', params);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('question')
+    return this.store.findAll('question');
+  },
+
+  actions: {
+    forgetIt () {
+      this.transitionTo('index');
+    },
+    addQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      console.log(newQuestion);
+      this.transitionTo('index');
+    }
   }
 });
