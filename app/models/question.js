37f439172687d93cbea47939c11e9157ff65afe2
matt knutson <mknut001@gmail.com>
2016-04-07 15:47:49 -0700

    add answer model

--- app/models/question.js
+++ app/models/question.js
@@ -4,4 +4,6 @@ export default DS.Model.extend({
   question: DS.attr(),
   author: DS.attr(),
   notes: DS.attr()
+  //// means that if the related model data isn't already loaded, an additional request should be made to Firebase in order to retrieve it ////
+  answers: DS.hasMany('answer', { async: true })
 });
