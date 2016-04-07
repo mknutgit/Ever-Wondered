861589fa3771458aa527edde924c7821e224057c
matt knutson <mknut001@gmail.com>
2016-04-07 16:44:29 -0700

    update answer model

--- app/models/question.js
+++ app/models/question.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   question: DS.attr(),
   author: DS.attr(),
-  notes: DS.attr()
+  notes: DS.attr(),
   //// means that if the related model data isn't already loaded, an additional request should be made to Firebase in order to retrieve it ////
   answers: DS.hasMany('answer', { async: true })
 });
