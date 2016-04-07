1133aad617d419faf3f84ff49f59a4afd285c16d
matt knutson <mknut001@gmail.com>
2016-04-07 13:56:43 -0700

    add add-question route

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'js-week3-cr-qa',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://qs-as.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
