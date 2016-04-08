19150c8598b2871ba253d18d203cf890c72279af
matt knutson <mknut001@gmail.com>
2016-04-08 13:46:57 -0700

    Able to get to answers

---
+++ app/components/add-answer.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+    forgetIt() {
+      this.set('addNewAnswer', false);
+    },
+    addAnswer() {
+      var params = {
+        answer: this.get('answer') ? this.get('answer'): "",
+        author: this.get('author') ? this.get('author'): ""
+      };
+      this.set('addNewAnswer', false);
+      this.set('answer', '');
+      this.set('author', '');
+      this.sendAction('addAnswer', params);
+    }
+  }
+});
--- app/models/answer.js
+++ app/models/answer.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   answer: DS.attr(),
   author: DS.attr(),
+  likes: DS.hasMany('likes', {async: true}),
   question: DS.belongsTo('question', { async: true})
 });
