8514eaa5f42137fff7b80c91b2f88aa2f1d2e032
matt knutson <mknut001@gmail.com>
2016-04-11 16:03:14 -0700

    add helper for questions with answer

---
+++ tests/unit/helpers/question-answer-test.js
@@ -0,0 +1,10 @@
+import { questionAnswer } from 'js-week3-cr-qa/helpers/question-answer';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | question answer');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = questionAnswer([42]);
+  assert.ok(result);
+});
