8061f8b909a034bea6b7e0f6a0bc6f34c0a1f555
matt knutson <mknut001@gmail.com>
2016-04-11 17:09:59 -0700

    update styling

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  
+  sortBy: ['answers.count:asc'],
+  sortedQuestions: Ember.computed.sort('questions', 'sortBy'),
+
   actions: {
     deleteAnswer(answer) {
       if (confirm('Are you sure you want to delete this rental?')) {
