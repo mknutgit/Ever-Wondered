723da052a5ba2df225bfe3f00934a99f636e96e7
Matt <mknut001@gmail.com>
2016-08-13 12:01:51 -0700

    add initial changes for github deploy

--- config/environment.js
+++ config/environment.js
@@ -42,7 +42,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.baseURL = '/js-week3-cr-qa/dist';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
