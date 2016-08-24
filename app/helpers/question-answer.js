03aef53967eb850fb24adb47d6ef8d4120bd280d
Matt <mknut001@gmail.com>
2016-08-24 10:14:41 -0700

    update glyphicon color

--- app/helpers/question-answer.js
+++ app/helpers/question-answer.js
@@ -4,7 +4,7 @@ export function questionAnswer(params/*, hash*/) {
   var question = params[0];
 
   if(question.get('answers').get('length') >= 2) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+    return Ember.String.htmlSafe('<p class="glyphicon glyphicon-fire"></p>');
   }
 }
 
