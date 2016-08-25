7849cbdc93e3e0708df458f019171cd2d4e0c1c5
Matt <mknut001@gmail.com>
2016-08-24 19:23:17 -0700

    Add additional helpers

--- app/helpers/question-answer.js
+++ app/helpers/question-answer.js
@@ -5,6 +5,8 @@ export function questionAnswer(params/*, hash*/) {
 
   if(question.get('answers').get('length') >= 2) {
     return Ember.String.htmlSafe('<p class="glyphicon glyphicon-fire"></p>');
+  } else if (question.get('answers').get('length') < 1) {
+    return Ember.String.htmlSafe('<p class="noAnswer">No answers yet!</p>');
   }
 }
 
