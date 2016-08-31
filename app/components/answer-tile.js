06f5df88f6b06ce9baa8436017a265228a79435d
Matt <mknut001@gmail.com>
2016-08-30 20:26:28 -0700

    Update styling and add answer computed property

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -4,6 +4,10 @@ export default Ember.Component.extend({
   sortBy: ['voteCount:desc'],
   sortedAnswers: Ember.computed.sort('answers', 'sortBy'),
 
+  totalAnswers: Ember.computed('answers.[]', function() {
+    return this.get('answers.length');
+  }),
+  
   updateAnswerFrom: false,
   actions: {
     upvote(answer) {
